931db80f8e2ad38cc6f65175b56ac3067b3c1f36
clinzy25 <conner.linzy25@gmail.com>
2021-08-03 12:44:36 -0400

    statick.json

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,20 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const pg = require("pg");
+pg.defaults.ssl = true;
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    'postgres://postgres:8150@localhost:5432/messenger',
+    "postgres://postgres:8150@localhost:5432/messenger",
   {
     logging: false,
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
