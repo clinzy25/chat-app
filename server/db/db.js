51d1d5b93fa2a9a060c9c79f608837c97faf9e9e
clinzy25 <conner.linzy25@gmail.com>
2021-08-03 09:04:38 -0400

    db connection options

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,17 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    "postgres://postgres:8150@localhost:5432/messenger?ssl=true",
+    "postgres://postgres:8150@localhost:5432/messenger",
   {
     logging: false,
+    options: {
+      dialect: "postgres",
+      ssl: true,
+      dialectOptions: {
+        ssl: true,
+      },
+    },
   }
 );
 
