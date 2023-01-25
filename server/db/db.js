63827f9185efb63790f0d14234bbba6a4ab946f8
clinzy25 <conner.linzy@area4labs.com>
2023-01-25 10:50:37 -0800

    update db conn

--- server/db/db.js
+++ server/db/db.js
@@ -3,19 +3,12 @@ const pg = require('pg')
 pg.defaults.ssl = true
 
 const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    'postgres://a4employee:8451@localhost:5432/messenger',
+  'postgres://a4employee:8451@localhost:5432/messenger',
   {
     logging: false,
     dialect: 'postgres',
-    ssl: true,
     dialectOptions: {
-      ssl: process.env.DATABASE_URL
-        ? {
-            require: true,
-            rejectUnauthorized: false,
-          }
-        : false,
+      ssl: false,
     },
   }
 )
