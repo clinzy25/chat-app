2d994657a8c1abf1d02fd89d6c991cde91b72eb3
clinzy25 <conner.linzy@area4labs.com>
2023-01-26 10:43:41 -0800

    use src dir for server, update dockerfiles

--- server/src/app.js
+++ server/src/app.js
@@ -53,7 +53,8 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
   res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? console.log(err) : {}
+  res.locals.error =
+    req.app.get('env') === 'development' ? console.log(err) : {}
 
   res.status(err.status || 500)
   res.json({ error: err })
