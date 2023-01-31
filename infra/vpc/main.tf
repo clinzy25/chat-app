bde1b7f7d8c9af46f9366c78076eb52544323ada
clinzy25 <conner.linzy@area4labs.com>
2023-01-31 11:18:12 -0800

    update vpc

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -15,17 +15,17 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = local.name
-  cidr = "10.0.0.0/24"
+  cidr = "10.0.0.0/22"
 
   azs              = ["${local.region}a", "${local.region}b"]
-  private_subnets  = ["10.0.0.0/26", "10.0.0.64/26"]
-  public_subnets   = ["10.0.0.128/28", "10.0.0.144/28"]
-  database_subnets = ["10.0.0.160/28", "10.0.0.176/28"]
-  # 10.0.0.191/26 available
-
-  private_subnet_names = ["private-${local.project}-subnet-1", "private-${local.project}-subnet-2"]
-  public_subnet_names  = ["public-${local.project}-subnet-1", "public-${local.project}-subnet-2"]
-  database_subnet_names  = ["db-${local.project}-subnet-1", "db-${local.project}-subnet-2"]
+  database_subnets  = ["10.0.0.0/26", "10.0.0.64/26"] # 128 IPs
+  public_subnets   = ["10.0.0.128/25", "10.0.1.0/25"] # 256 IPs
+  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"] # 512 IPs
+  # 896 total IPs, 128 free
+  
+  private_subnet_names = ["private-1-${local.project}-subnet", "private-2-${local.project}-subnet"]
+  public_subnet_names  = ["public-1-${local.project}-subnet", "public-2-${local.project}-subnet"]
+  database_subnet_names  = ["db-1-${local.project}-subnet", "db-2-${local.project}-subnet"]
 
   create_database_subnet_group = true
 
