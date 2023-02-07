3d2881120447948eb2b3dfbc20b3ff4a14e5cc49
clinzy25 <conner.linzy@area4labs.com>
2023-02-07 08:27:47 -0800

    configure envs for seed

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,15 +1,23 @@
 const Sequelize = require('sequelize')
+require('dotenv').config('../../.env')
 
 const {
   POSTGRES_DATABASE,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
+  POSTGRES_USER_PROD,
+  POSTGRES_PASSWORD_PROD,
+  POSTGRES_USER_DEV,
+  POSTGRES_PASSWORD_DEV,
   POSTGRES_HOST_PROD,
   POSTGRES_PORT_PROD,
   POSTGRES_HOST_DEV,
   POSTGRES_PORT_DEV,
   NODE_ENV,
 } = process.env
+
+const username =
+  NODE_ENV === 'production' ? POSTGRES_USER_PROD : POSTGRES_USER_DEV
+const password =
+  NODE_ENV === 'production' ? POSTGRES_PASSWORD_PROD : POSTGRES_PASSWORD_DEV
 
 const config =
   NODE_ENV === 'production'
@@ -18,7 +26,7 @@ const config =
         port: POSTGRES_PORT_PROD,
         dialect: 'postgres',
         dialectOptions: {
-          ssl: 'Amazon RDS',
+          rejectUnauthorized: false,
         },
       }
     : {
@@ -27,11 +35,6 @@ const config =
         dialect: 'postgres',
       }
 
-const db = new Sequelize(
-  POSTGRES_DATABASE,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  config
-)
+const db = new Sequelize(POSTGRES_DATABASE, username, password, config)
 
 module.exports = db
